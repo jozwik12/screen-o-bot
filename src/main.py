a71130ce763c7d88b513e12cf689e16d0ff56940
jozwik12 <szymonjozwicki@gmail.com>
2021-09-29 14:12:38 +0200

    restructure code into functions

--- src/main.py
+++ src/main.py
@@ -3,17 +3,28 @@ import time
 
 gui.PAUSE = 1
 gui.FAILSAFE = True
-counter = 1
+counter = 0
+time_between_screenshots = 2
 
-while True:
-    try:
-        myscreenshot = gui.screenshot()
-        myscreenshot.save(fr"C:\Users\TITAN\Desktop\pyscreens\screenshot_{counter}.png")
-        counter += 1
-        time.sleep(2)
-        x, y = gui.position()
-        if x == 0 and y == 0:
-            raise gui.FailSafeException
-    except gui.FailSafeException:
-        gui.alert("Program has been stopped")
-        exit()
+
+def take_screenshot():
+    myscreenshot = gui.screenshot()
+    global counter
+    counter += 1
+    myscreenshot.save(fr"C:\Users\TITAN\Desktop\pyscreens\screenshot_{counter}.png")
+    time.sleep(time_between_screenshots)
+
+
+def main_loop():
+    while True:
+        try:
+            take_screenshot()
+            x, y = gui.position()
+            if x == 0 and y == 0:
+                raise gui.FailSafeException
+        except gui.FailSafeException:
+            gui.alert("Program has been stopped")
+            exit()
+
+
+main_loop()
