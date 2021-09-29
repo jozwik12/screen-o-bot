cde07349114511ae58576b7c2a6302c99b1d4f1b
jozwik12 <szymonjozwicki@gmail.com>
2021-09-29 16:37:30 +0200

    add function comparing current screen to previous screenshot

--- src/main.py
+++ src/main.py
@@ -16,6 +16,22 @@ def take_screenshot():
     time.sleep(time_between_screenshots)
 
 
+"""
+Checks if current screen state is similar to latest screenshot
+returns true if there is difference between current, and previous screenshot
+    meaning - screenshot should be taken, as screen state has changed
+returns false if previous screenshot is the same as current screen state
+"""
+def compare_screenshots():
+    global counter
+    try:
+        loc = gui.locateOnScreen(fr"C:\Users\TITAN\Desktop\pyscreens\screenshot_{counter}.png",
+                                 confidence=comparison_confidence)
+        return not bool(loc)
+    except gui.ImageNotFoundException:
+        return True
+
+
 def check_for_program_termination():
     x, y = gui.position()
     if x == 0 and y == 0:
