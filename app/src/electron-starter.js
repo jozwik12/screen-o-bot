87fea34d7d6d004ca4a37fa00aeab53fad7eba40
jozwik12 <szymonjozwicki@gmail.com>
2021-11-27 20:00:37 +0100

    Add "loading..." window title before index.html loads

--- app/src/electron-starter.js
+++ app/src/electron-starter.js
@@ -8,6 +8,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Loading...",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
