984b37c867d8750301d990a0a8c6e09106170d30
jozwik12 <szymonjozwicki@gmail.com>
2022-02-25 16:49:06 +0100

    remove unnecessary files from repo

--- app/backend/src/coordinates.py
+++
@@ -1,9 +0,0 @@
-import time
-import pyautogui
-
-while True:
-    time.sleep(1)
-    print(pyautogui.position())
-    x, y = pyautogui.position()
-    if x == 0 and y == 0:
-        exit()
--- app/backend/src/test.py
+++
@@ -1,7 +0,0 @@
-import sys
-import json
-import time
-
-input = json.load(sys.stdin)
-
-print(json.dumps(input))
