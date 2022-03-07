d22d72ced511282031fcd0310e08efe9f3bd682d
jozwik12 <szymonjozwicki@gmail.com>
2022-03-07 08:38:59 +0100

    make mainWindow always on top but minimizable

--- electron/electron.js
+++ electron/electron.js
@@ -110,6 +110,7 @@ const createMainWindow = () => {
     width: 800,
     height: 600,
     title: "Loading...",
+    alwaysOnTop: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       nativeWindowOpen: true,
