248544b4a42273fe89ef3b083573fcc5eb636ffa
jozwik12 <szymonjozwicki@gmail.com>
2022-04-12 22:40:33 +0200

    add more reactour steps

--- src/tourSteps.jsx
+++ src/tourSteps.jsx
@@ -6,19 +6,18 @@ const steps = [
   },
   {
     selector: ".RecorderButton",
-    content: "Tym przyciskiem uruchomisz nagrywanie",
+    content:
+      "Tym przyciskiem otworzysz okno za pomocą którego wybierzesz jaki obszar ekranu będzie przechwytywany. Spróbuj teraz!",
   },
   {
-    selector: ".Help",
-    content: "Tutaj dowiesz się jak używać programu",
+    selector: ".RecorderButton",
+    content:
+      "Okno możesz swobodnie przeciągać i zmieniać rozmiar. Przechwycony zostanie obszar wewnątrz czerwonej ramki. Naciśnij przycisk ponownie aby rozpocząć nagrywanie",
   },
   {
-    selector: ".ProjectInfo",
-    content: "Kliknij tutaj, aby dowiedzieć się więcej o projekcie",
-  },
-  {
-    selector: ".Disclaimer",
-    content: "Aby wyświetlić ponownie zastrzeżenie autora, kliknij tutaj",
+    selector: ".RecorderButton",
+    content:
+      "Nagrywanie trwa. Aby je przerwać naciśnij przycisk ponownie. Folder ze screenshotami otworzy się automatycznie",
   },
   {
     selector: ".SavePath",
@@ -28,6 +27,18 @@ const steps = [
   {
     selector: ".ChooseSavePath",
     content: "Tym przyciskiem możesz zmienić ścieżkę zapisu",
+  },
+  {
+    selector: ".Help",
+    content: "Pod tym przyciskiesz znajdziesz krótki tutorial obsługi programu",
+  },
+  {
+    selector: ".ProjectInfo",
+    content: "Kliknij tutaj, aby dowiedzieć się więcej o projekcie",
+  },
+  {
+    selector: ".Disclaimer",
+    content: "Aby wyświetlić ponownie zastrzeżenie autora, kliknij tutaj",
   },
 ];
 
